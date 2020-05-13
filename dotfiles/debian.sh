10180d64c1779f40e3b768ebf9a4dc8889698abe
degnbol <christian.degnbol@gmail.com>
2020-05-13 20:15:02 +0200

    more editors to install

--- dotfiles/debian.sh
+++ dotfiles/debian.sh
@@ -9,9 +9,21 @@ sudo apt install python-pip python3-pip
 
 sudo apt install fonts-powerline
 
+sudo apt install r-base
+sudo apt install julia
+# install Rstudio from https://rstudio.com/products/rstudio/download/#download
+
 # power management
 sudo apt install laptop-mode-tools
+sudo apt install linux-cpupower
 
-# maybe java development kit?
 sudo apt install default-jdk
+
+# snap app store for linux with non typical apps such as pycharm
+sudo apt install snapd
+# classic means not in sandbox
+sudo snap install pycharm-community --classic
+sudo snap install code --classic
+
+sudo snap refresh
 
