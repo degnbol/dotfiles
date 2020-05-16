30e1e4e36179eed08712230aef9f90431f321aa9
degnbol <christian.degnbol@gmail.com>
2020-05-16 18:04:20 +0200

    nvim installation help

---
+++ dotfiles/nvim.sh
@@ -0,0 +1,5 @@
+# open vi and write :PlugInstall to install plugins
+vi
+# then complete the youcompleteme installation (here with c family and java support)
+python3 ~/.config/nvim/plugged/youcompleteme/install.py --clangd-completer --java-completer
+
