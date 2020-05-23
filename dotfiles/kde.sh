a77be3f3b4b8080dfd7a6bb849cc4a5d377fe5d1
degnbol <christian.degnbol@gmail.com>
2020-05-23 22:40:01 +0200

    3 fingers up to present windows

--- dotfiles/kde.sh
+++ dotfiles/kde.sh
@@ -13,4 +13,12 @@ _ sed -Ei 's/;? ?flat-volumes.*/flat-volumes = no/' /etc/pulse/daemon.conf
 systemctl --user restart pulseaudio.service
 systemctl --user restart pulseaudio.socket
 
-
+# trying to make touchpad better
+# runtime config of Xinput devices
+_ apt install xinput
+# https://bill.harding.blog/2018/01/07/linux-with-a-macbook-touchpad-feel-pt-2/
+_ apt install xserver-xorg-input-synaptics
+# you should be able to set settings in KDE system settings -> input devices -> touchpad.
+# disable mouse click emulation, set vertical scrolling to 60 mm and 7 mm, disable coasting and circular scrolling, set min pointer speed =0, and max 1/3 of full, about the same for acceleration.
+# set min pressure factor 0, noise cancellation 50.
+# also install https://github.com/bulletmark/libinput-gestures so you can e.g. swipe 3 fingers up to present windows
