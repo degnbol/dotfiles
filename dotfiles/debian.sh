df5303d0246198ae5172c8c397ea1faf5a79ff48
degnbol <christian.degnbol@gmail.com>
2020-05-27 00:24:11 +0200

    install mendeley

--- dotfiles/debian.sh
+++ dotfiles/debian.sh
@@ -14,6 +14,9 @@ _ apt install flatpak -y
 # add repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+_ flatpak install mendeley
+
+
 _ apt install fonts-powerline
 
 _ apt install neovim -y
