6734d41513166906fe35193b8e63ba1560553625
degnbol <christian.degnbol@gmail.com>
2020-06-08 22:30:49 +0200

    adding boot loader rEFInd with a minimal theme

--- dotfiles/debian.sh
+++ dotfiles/debian.sh
@@ -68,5 +68,13 @@ _ apt install fonts-firacode
 # go to https://github.com/tonsky/FiraCode to find install instructions for various IDEs. 
 # install Dark ++ Italic theme in vc code
 
-
+# bootloader rEFInd, press yes to automatically installing.
+_ apt install refind
+# then install a minimalistic theme in the folder where the rEFInd executable is located, e.g.
+_ ll /boot/efi/EFI/refind/  # assuming this is the location.
+_ mkdir /boot/efi/EFI/refind/themes
+_ git clone https://github.com/EvanPurkhiser/rEFInd-minimal.git /boot/efi/EFI/refind/themes/rEFInd-minimal
+# then add the line "include themes/rEFInd-minimal/theme.conf" at the end:
+_vi /boot/efi/EFI/refind/refind.conf
+# you can remove "tags" (boot entries) by pressing minus in the boot menu. They can also be restored in the boot menu but not when the minimal theme is active.
 
